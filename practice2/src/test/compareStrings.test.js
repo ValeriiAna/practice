2243404cac3038756c9ddae8db013d65ce9318f5
ValeriiAna <95766059+ValeriiAna@users.noreply.github.com>
2021-12-27 12:51:41 +0200

    Add files via upload

--- practice2/src/test/compareStrings.test.js
+++ practice2/src/test/compareStrings.test.js
@@ -1,5 +1,25 @@
 const compareStrings = require('../compareStrings');
 
 describe('compareStrings', () => {
-    test()
+    test('compareStrings', () => {
+        expect(true).toEqual(true);
+    })
+    test('compareStrings', ()=> {
+        expect(compareStrings('World', 'lorwd')).toEqual(true);
+    })
+    test('compareStrings', () => {
+        expect(compareStrings(2345676, 'kotok')).toEqual('Not string');
+    })
+    test('compareStrings', () => {
+        expect(compareStrings(' ', 'interesT')).toEqual(false);
+    })
+    test('compareStrings', () => {
+        expect(compareStrings('KOn$$akt', 'KONTAKT')).toEqual(false);
+    })
+    test('compareStrings', () => {
+        expect(compareStrings('FAntacY', 'anTACYf')).toEqual(true);
+    })
+test('compareStrings', () => {
+    expect(compareStrings('javaSCript', 'SCRIpTava')).toEqual(false);
+})
 })
